afd768d476dd5b078eb9dcd9947bd55cf6d008a8
Wanderson Ricardo <wanderson.silva@gec.inatel.br>
2022-03-10 21:43:35 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,33 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 function Listing() {
-    return(
-        <Pagination />
+    return (
+        <>
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-md-3 col-lg-4 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-md-3 col-lg-4 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-md-3 col-lg-4 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-md-3 col-lg-4 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-md-3 col-lg-4 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-md-3 col-lg-4 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
